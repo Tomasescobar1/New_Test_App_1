c5ff2e27fa97f9d060dd47ee365d37251c25e744
Tomás Escobar Ruiz <thomasatlascoporation@gmail.com>
2024-08-30 18:18:55 -0500

    Added about page to views

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   root 'pages#home'
+  get 'about', to: 'pages#about'
 
   # Defines the root path route ("/")
   # root "posts#index"
